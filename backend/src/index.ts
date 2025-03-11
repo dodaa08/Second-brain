4fdb1dcbe56968be8e7e243d7381bd65cd78cdaa
dodaa08 <dodakartik26@gmail.com>
2025-03-12 00:39:20 +0530

    deployments

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -23,7 +23,7 @@ app.use(express.json());
 
 
 const corsOption = {
-    origin: ['https://thinkkk.vercel.app/'],  // ✅ Remove trailing slash
+    origin: ['https://thinkkk.vercel.app'],  // ✅ Remove trailing slash
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 };
